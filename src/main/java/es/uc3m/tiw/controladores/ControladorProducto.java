b973bacfe98fad2e39ec9fad93103bd05ca91060
norsan03 <100292913@alumnos.uc3m.es>
2016-12-11 18:03:38 +0000

    Añadido Controlador Producto

--- src/main/java/es/uc3m/tiw/controladores/ControladorProducto.java
+++ src/main/java/es/uc3m/tiw/controladores/ControladorProducto.java
@@ -1,5 +1,37 @@
 package es.uc3m.tiw.controladores;
 
-public class ControladorProducto {
+import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.client.RestTemplate;
+
+import es.uc3m.tiw.dominios.*;
+
+@Controller
+public class ControladorProducto {
+	
+	@Autowired
+	RestTemplate restTemplate;
+	
+
+	
+	@RequestMapping(value="/altaProducto", method=RequestMethod.GET)
+	public String crearProducto(Model modelo){
+		return "altaProducto";
+	}
+	
+	@RequestMapping(value="/altaProducto", method=RequestMethod.POST)
+	public String guardarProducto(Model modelo, @ModelAttribute Producto producto){
+		Producto pregistrado = restTemplate.postForObject("http://localhost:8020/altaProducto", producto, Producto.class);
+		modelo.addAttribute(pregistrado);
+		return "misProductos";
+	}
+
+	
 }
